fb9c6ff87c2f91c90ebad4d289904c0c82385389
edilsonmorais <edilsonmorais16@gmail.com>
2022-10-02 19:53:46 -0300

    Alteração na classe App

--- src/trilha/java/App.java
+++ src/trilha/java/App.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trilha.java;
 
 /**
@@ -10,5 +6,7 @@ package trilha.java;
  * @author edils
  */
 public class App {
-    
+    public static void main(String[] args) {
+        System.out.println("Iniciando os Estudos em Java pela DIO");
+    }
 }
